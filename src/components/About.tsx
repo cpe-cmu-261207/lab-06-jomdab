ad6463459e87d1f789e86fc0391e4bea33b3745b
jomdab <shupak999@gmail.com>
2021-08-29 23:22:58 +0700

    what???

---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+
+const About = () => {
+    return(
+        <div className='text-center space-y-3'>
+            <p className='text-2xl font-semibold'>About me</p>
+            <p className='text-xl'>Supak Chainetkasem 630610769</p>
+          </div>
+    )
+}
+export default About
